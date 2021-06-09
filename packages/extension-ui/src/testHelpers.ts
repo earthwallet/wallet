7b2dcf56fff2f14c96e53ad59353af0de3d17e18
earthgohan <pavan@social.network>
2021-06-10 03:02:02 +0530

    Update tests

--- packages/extension-ui/src/testHelpers.ts
+++ packages/extension-ui/src/testHelpers.ts
@@ -1,5 +1,6 @@
 // Copyright 2021 @earthwallet/extension-ui authors & contributors
 // SPDX-License-Identifier: Apache-2.0
+require("setimmediate");  
 
 export function flushAllPromises (): Promise<void> {
   return new Promise((resolve) => setImmediate(resolve));
